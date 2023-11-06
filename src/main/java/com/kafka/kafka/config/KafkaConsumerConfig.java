f332644ec4915f8719f696e3a1b005a466de012f
memorytao <memorytao@gmail.com>
2023-11-06 20:46:45 +0700

    add consumer

--- src/main/java/com/kafka/kafka/config/KafkaConsumerConfig.java
+++ src/main/java/com/kafka/kafka/config/KafkaConsumerConfig.java
@@ -4,10 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Consumer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
